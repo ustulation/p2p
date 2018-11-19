44eaa60be35d3177064c24a7c2d6c68da04edc17
Povilas Balciunas <balciunas90@gmail.com>
2018-11-19 17:21:51 +0200

    fix/docs: fix mio types we use

--- src/lib.rs
+++ src/lib.rs
@@ -346,7 +346,7 @@ impl NatTimer {
 /// A message that can be sent to the event loop to perform an action.
 ///
 /// This can be used to send actions from a thread outside the event loop too if sent via
-/// [`mio::channel::Sender`][0].
+/// [`mio_extras::channel::Sender`][0].
 ///
 /// [0]: http://rust-doc.s3-website-us-east-1.amazonaws.com/mio/master/mio/channel
 pub struct NatMsg(Box<FnMut(&mut Interface, &Poll) + Send + 'static>);
@@ -408,8 +408,8 @@ pub trait Interface {
     fn remove_state(&mut self, token: Token) -> Option<Rc<RefCell<NatState>>>;
     /// Return the state (without removing - just a query) associated with the `token`
     fn state(&mut self, token: Token) -> Option<Rc<RefCell<NatState>>>;
-    /// Set timeout. User code is expected to have a `mio::timer::Timer<NatTimer>` on which the
-    /// timeout can be set.
+    /// Set timeout. User code is expected to have a `mio_extras::timer::Timer<NatTimer>` on which
+    /// the timeout can be set.
     fn set_timeout(&mut self, duration: Duration, timer_detail: NatTimer) -> Timeout;
     /// Cancel the timout
     fn cancel_timeout(&mut self, timeout: &Timeout) -> Option<NatTimer>;
